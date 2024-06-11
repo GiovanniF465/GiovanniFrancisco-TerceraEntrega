8cf8bfa7f94a3f21a3b583bcb086a4a0eae643b4
Giovanni Francisco Perez <giovanni.francisco@cognitive.mx>
2024-06-10 21:04:11 -0600

    Se ha implementado la plantilla descargarda como página de inicio del proyecto

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from main import views
 
 urlpatterns = [
-    
+    path('',views.inicio),
+    path('empleados/nuevoEmpleado/',views.empleadoNuevo)
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Empleado
 
-def inicio (request):
-    ...
+def inicio(request):
+    return render(request,'main/index.html')
+
+def empleadoNuevo (request,nombre,puesto,correoE):
+    e = Empleado(nombre=nombre,puesto=puesto,correoE=correoE)
+    e.save()
+    return render(request,'creacion.html',{"Empleado " : e})
